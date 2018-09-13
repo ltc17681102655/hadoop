70b820747b1e4b4c7b77a2e925809cc2845119a7
ltcGit <17681102655@163.com>
2018-09-13 21:58:17 +0800

    创建scala项目

---
+++ src/scala/Language.scala
@@ -0,0 +1,12 @@
+package scala
+/**
+ * @Auther: ltc
+ * @Date: 2018/9/13 21:54
+ * @Description: scala 基本语法
+ */
+class Language {
+//def main ==>main 方法
+  def main(args: Array[String]): Unit = {
+    println("hello scala");
+  }
+}
